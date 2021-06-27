7ea63d5955dceb2e5236557171ebeec889654fae
pratik kagale <pratikkagale3333@gmail.com>
2021-06-26 20:12:52 -0700

    Creating Homepage of our TextUtils Website

--- textutils2/textutils2/views.py
+++ textutils2/textutils2/views.py
@@ -2,10 +2,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def index(request):
-    params={'name':'Pratik','place':'India'}
-    return render(request,'index.html',params)
+    return render(request,'index.html')
 
 def removepunc(request):
+    #Get the text
+    djtext=request.GET.get('text', 'default')
+    print(djtext)
+    #analyze the text
     return HttpResponse("<h1>Remove punc</h1>")
 
 def capitalizefirst(request):
